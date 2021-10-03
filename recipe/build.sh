ffbb28a28efa8c4eac683367be10fa225046befd
Duncan Macleod <duncan.macleod@ligo.org>
2021-10-03 08:49:46 +0100

    enable osx arm64 migration
    and update recipe for cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,16 +7,18 @@ pushd _build
 cmake \
 	${SRC_DIR} \
 	${CMAKE_ARGS} \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DENABLE_SWIG_PYTHON2=no \
-	-DENABLE_SWIG_PYTHON3=no \
+	-DCMAKE_BUILD_TYPE:STRING=Release \
+	-DENABLE_SWIG_PYTHON2:BOOL=no \
+	-DENABLE_SWIG_PYTHON3:BOOL=no \
 ;
 
 # build
 cmake --build . --parallel ${CPU_COUNT} --verbose
 
 # test
-ctest --parallel ${CPU_COUNT} --verbose
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+	ctest --parallel ${CPU_COUNT} --verbose
+fi
 
 # install
 cmake --build . --parallel ${CPU_COUNT} --verbose --target install
